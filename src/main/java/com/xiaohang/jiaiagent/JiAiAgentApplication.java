e9f08449d65140e73d15357cb0e828830848ca39
tokamaky <jxh186045@gmail.com>
2026-04-09 18:17:20 -0400

    initial project

---
+++ src/main/java/com/xiaohang/jiaiagent/JiAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.xiaohang.jiaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xiaohang/jiaiagent/JiAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaohang.jiaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

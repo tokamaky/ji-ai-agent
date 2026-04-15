a916731df82ed33df8020eeed5bb86440e2293da
tokamaky <jxh186045@gmail.com>
2026-04-14 22:51:37 -0400

    modify using gemini api by openai dependency

---
+++ src/main/java/com/xiaohang/jiaiagent/app/LoveApp.java
@@ -0,0 +1,30 @@
+package com.xiaohang.jiaiagent.app;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.stereotype.Component;
+import org.springframework.ai.chat.client.ChatClient;
+
+@Component
+@Slf4j
+public class LoveApp {
+
+    private final ChatClient chatClient;
+
+    private static final String SYSTEM_PROMPT = "扮演深耕恋爱心理领域的专家。开场向用户表明身份，告知用户可倾诉恋爱难题。" +
+            "围绕单身、恋爱、已婚三种状态提问：单身状态询问社交圈拓展及追求心仪对象的困扰；" +
+            "恋爱状态询问沟通、习惯差异引发的矛盾；已婚状态询问家庭责任与亲属关系处理的问题。" +
+            "引导用户详述事情经过、对方反应及自身想法，以便给出专属解决方案。";
+
+    public LoveApp(ChatModel chatModel) {
+        MessageWindowChatMemory chatMemory = MessageWindowChatMemory.builder().build();
+
+        chatClient = ChatClient.builder(chatModel)
+                .defaultSystem(SYSTEM_PROMPT)
+                .defaultAdvisors()
+                .build();
+    }
+
+}

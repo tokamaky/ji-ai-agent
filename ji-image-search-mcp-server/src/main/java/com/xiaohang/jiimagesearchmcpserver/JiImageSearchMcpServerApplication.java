aa9f523b2d75b8da74ed4ed79105bc73a0b16708
tokamaky <jxh186045@gmail.com>
2026-04-20 19:39:35 -0400

    create mcp project

---
+++ ji-image-search-mcp-server/src/main/java/com/xiaohang/jiimagesearchmcpserver/JiImageSearchMcpServerApplication.java
@@ -0,0 +1,13 @@
+package com.xiaohang.jiimagesearchmcpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiImageSearchMcpServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiImageSearchMcpServerApplication.class, args);
+    }
+
+}

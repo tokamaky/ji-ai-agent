b0716f708a12cb3095d67bb83464fe21683fb4a0
tokamaky <jxh186045@gmail.com>
2026-04-20 11:41:53 -0400

    add ToolRegistration

--- src/main/java/com/xiaohang/jiaiagent/tools/WebSearchTool.java
+++ src/main/java/com/xiaohang/jiaiagent/tools/WebSearchTool.java
@@ -7,8 +7,6 @@ import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.ai.tool.annotation.ToolParam;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.List;
@@ -18,14 +16,13 @@ import java.util.stream.Collectors;
 /**
  * 网页搜索工具
  */
-@Component
 public class WebSearchTool {
 
     private static final String SEARCH_API_URL = "https://www.searchapi.io/api/v1/search";
 
     private final String apiKey;
 
-    public WebSearchTool(@Value("${search.api-key}") String apiKey) {
+    public WebSearchTool(String apiKey) {
         this.apiKey = apiKey;
     }
 

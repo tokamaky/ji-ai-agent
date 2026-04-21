62cca9c5c191756a5487590dfa5ff13de0d02024
tokamaky <jxh186045@gmail.com>
2026-04-20 22:34:08 -0400

    create image-mcp-server

--- ji-image-search-mcp-server/src/main/java/com/xiaohang/jiimagesearchmcpserver/JiImageSearchMcpServerApplication.java
+++ ji-image-search-mcp-server/src/main/java/com/xiaohang/jiimagesearchmcpserver/JiImageSearchMcpServerApplication.java
@@ -1,13 +1,24 @@
 package com.xiaohang.jiimagesearchmcpserver;
 
+import com.xiaohang.jiimagesearchmcpserver.tools.ImageSearchTool;
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class JiImageSearchMcpServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(JiImageSearchMcpServerApplication.class, args);
+    }
+
+    @Bean
+    public ToolCallbackProvider imageSearchTools(ImageSearchTool imageSearchTool) {
+        return MethodToolCallbackProvider.builder()
+                .toolObjects(imageSearchTool)
+                .build();
     }
 
 }
